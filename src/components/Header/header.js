a0d14bcf09ffdadb15ee2b8347f94f7bdf4c7ffe
Aleksandr Zidyganov <alex@csscoder.pro>
2020-09-29 18:45:38 +0500

    add smooth-scroll plugin and small fixes

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
   $("#sticker").sticky({topSpacing: -20});
+
+  $('.js-goto').click(function(event) {
+    event.preventDefault();
+    var link = this;
+    $.smoothScroll({
+      offset: -80,
+      scrollTarget: link.hash
+    });
+  });
 });
